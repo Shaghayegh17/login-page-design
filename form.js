6dfe0e1971f446058608e26ac6b8cb1da2313e2b
shaghayeghghadimi <shaghayeghghadimi99@gmail.com>
2026-06-13 18:57:09 +0330

    radio and checkbox events done and some change for focus events done

--- form.js
+++ form.js
@@ -6,6 +6,9 @@ form.addEventListener("submit", (event) => {
   console.log(form.elements.fullname.value);
   console.log(form.elements.email.value);
   console.log(form.elements.password.value);
+  console.log(form.elements.rule.value);
+  console.log(form.elements.gender.value);
+  console.log(form.elements.gender);
   console.log(event.target);
   console.log(event.type);
 });
@@ -14,20 +17,21 @@ form.addEventListener("submit", (event) => {
 // console.log(email);
 // ما به مقدار فرم هم میتونیم دسترسی پیدا کنیم
 // غیرفعال کردن یک بخش فرم مثلا قبل اینکه بخش دیگه انجام بشه
-form.elements.password.disable = true;
+// form.elements.password.disabled = true;
 // برای جلب توجه کاربر به یک بخش فرم
-setTimeout(() => {
-  form.elements.email.focus();
-}, 3000);
+// setTimeout(() => {
+//   form.elements.email.focus();
+// }, 3000);
 // برای دراوردن از فوکس
-setTimeout(() => {
-  form.elements.email.blur();
-}, 6000);
+// setTimeout(() => {
+//   form.elements.email.blur();
+// }, 6000);
 // این ها خودشون میتونن ایونت باشن
 // form.elements.password.addEventListener("focus", () => {
 //   password.style.border = "2px solid red";
 // });
 // در این حالت برای همشون اعمال میشه
+
 for (let input of form.elements) {
   input.addEventListener("focus", () => {
     input.style.border = "2px solid red";
@@ -36,16 +40,50 @@ for (let input of form.elements) {
 
 for (let part of form.elements) {
   part.addEventListener("change", () => {
-    part.style.backgroundColor = "lightgreen";
+    part.style.border = "2px solid rgb(58, 238, 58)";
   });
 }
 
 // trim() فاصله‌های اول و آخر را حذف می‌کند. در این صورت اگه اسپیس بزنه خارج بشه باز الارم و میبینه
 
-form.elements.email.addEventListener("blur", () => {
-  if ((email.value.trim = " ")) {
-    alert("ایمیل خود را وارد کنید");
+// form.elements.email.addEventListener("blur", () => {
+//   if ((email.value.trim() = " ")) {
+//     alert("ایمیل خود را وارد کنید");
+//   }
+// });
+// با روش زیر دیگه گیر نمیکنه رو آلرت و میره بخش بعد
+// برای هر بلور، باید شرط بزاری قبل آلرت دادن
+for (let all of form.elements) {
+  if (all.type === "email" || "text" || "password")
+    all.addEventListener("blur", () => {
+      if (all.value.trim() === "") {
+        alert("فیلد انتخاب شده را حتما پر کنید");
+      }
+    });
+}
+// radio
+// first way
+// for (let gend of form.elements.gender) {
+//   gend.addEventListener("change", (event) => {
+//     console.log(event.target.value);
+//   });
+// }
+// second way
+form.addEventListener("change", (event) => {
+  if (event.target.name === "gender") {
+    console.log(event.target.value);
   }
 });
+// کل ردیوباتن با کد زیر نمایش داده میشه
+// form.elements.gender.value
+// برای نمایش مقدارانتخاب شده
+// event.target.checked
 
- 
+// checkbox
+form.addEventListener("change", (event) => {
+  if (event.target.name === "rule") {
+    console.log(event.target.checked);
+  }
+});
+// rule.checked = true;
+// در حالت بالا، تیک رول خودکار زده میشه
